425b690e8658583e736c656408a3770e508d72a2
rgupta1234 <rkgupta75@gmail.com>
2018-05-03 08:49:42 -0500

    Update run.sh
    aa

--- pre-install/run.sh
+++ pre-install/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook /home/ec2-user/openshift-install/setup.yml -e rhsm_username=$RSHM_USER -e rhsm_pool=$RHSM_POOL "$@"
+ansible-playbook /home/ec2-user/openshift-install-toolset/pre-install/setup.yml -e rhsm_username=$RSHM_USER -e rhsm_pool=$RHSM_POOL "$@"
